9da12ae628d8d8cdb831da16029f510338999bb8
albert <>
2002-10-27 10:44:05 +0000

    LGPL

--- pmap.c
+++ pmap.c
@@ -1,3 +1,14 @@
+/*
+ * Copyright 1998 by Albert Cahalan; all rights reserved.
+ * This file may be used subject to the terms and conditions of the
+ * GNU Library General Public License Version 2, or any later version
+ * at your option, as published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Library General Public License for more details.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
