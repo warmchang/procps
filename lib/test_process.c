420cd9c7c23b558491e7aa99f8a258c1421e867c
Craig Small <csmall@enc.com.au>
2015-06-13 15:04:31 +1000

    Create test process
    For the test suite, procps used to use sleep which would just
    create a process or two to test the tools against.  Some setups
    coreutils creates all programs including sleep into one blob which
    means a lot of the tests fail, see issue #2
    
    procps has its own sleep program now.

---
+++ lib/test_process.c
@@ -0,0 +1,88 @@
+/*
+ * test_procps -- program to create a process to test procps
+ *
+ * Copyright 2015 Craig Small <csmall@enc.com.au>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/prctl.h>
+#include "c.h"
+
+#define DEFAULT_SLEEPTIME 300
+#define MY_NAME "spcorp"
+
+static void usage(void)
+{
+    fprintf(stderr, " %s [options]\n", program_invocation_short_name);
+    fprintf(stderr, " -s <seconds>\n");
+    exit(EXIT_FAILURE);
+}
+
+
+void
+signal_handler(int signum)
+{
+    switch(signum) {
+	case SIGUSR1:
+	    printf("SIG SIGUSR1\n");
+	    break;
+	case SIGUSR2:
+	    printf("SIG SIGUSR2\n");
+	    break;
+	default:
+	    printf("SIG unknown\n");
+	    exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int sleep_time, opt;
+    struct sigaction signal_action;
+
+    sleep_time = DEFAULT_SLEEPTIME;
+    while ((opt = getopt(argc, argv, "s:")) != -1) {
+	switch(opt) {
+	    case 's':
+		sleep_time = atoi(optarg);
+		if (sleep_time < 1) {
+		    fprintf(stderr, "sleep time must be 1 second or more\n");
+		    usage();
+		}
+		break;
+	    default:
+		usage();
+	}
+    }
+
+    /* Setup signal handling */
+    signal_action.sa_handler = signal_handler;
+    sigemptyset (&signal_action.sa_mask);
+    signal_action.sa_flags = 0;
+    sigaction(SIGUSR1, &signal_action, NULL);
+    sigaction(SIGUSR2, &signal_action, NULL);
+
+    /* set process name */
+    prctl(PR_SET_NAME, MY_NAME, NULL, NULL, NULL);
+
+    while (sleep_time > 0) {
+	sleep_time = sleep(sleep_time);
+    }
+    return EXIT_SUCCESS;
+}
