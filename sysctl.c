2617d03a33a2e68a6ee6fed2278fcac6a3fb59b4
Victor Ananjevsky <ananasik@gmail.com>
2015-06-26 23:03:11 +1000

    sysctl: Correct stat on /etc/sysctl.conf
    sysctl --system fails when the file /etc/sysctl.conf doesn't
    exists. This happens due to wrong check of stat(2) return code.
    
    Reference:
      https://www.freelists.org/post/procps/sysctl
    
    Signed-off-by: Craig Small <csmall@enc.com.au>

--- sysctl.c
+++ sysctl.c
@@ -655,7 +655,7 @@ static int PreloadSystem(void)
 	}
 
 
-	if (stat(DEFAULT_PRELOAD, &ts) < 0 || S_ISREG(ts.st_mode)) {
+	if (stat(DEFAULT_PRELOAD, &ts) == 0 && S_ISREG(ts.st_mode)) {
 		if (!Quiet)
 			printf(_("* Applying %s ...\n"), DEFAULT_PRELOAD);
 		rc |= Preload(DEFAULT_PRELOAD);
