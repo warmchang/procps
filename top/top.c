5e33325f9a36bd5acb6726c1243a0e46d6623dbc
Jim Warner <james.warner@comcast.net>
2016-07-03 00:00:00 -0500

    top: improved that #define PRETENDNOCAP implementation
    This development (only) define can be used to turn top
    into a simple text program, disabling termcap effects.
    But input (at screen bottom) suffers from a line wrap.
    
    So, this commit just makes the input prompt processing
    a little more effective by adding one leading newline.
    
    Signed-off-by: Jim Warner <james.warner@comcast.net>

--- top/top.c
+++ top/top.c
@@ -752,7 +752,11 @@ static void show_msg (const char *str) {
 static int show_pmt (const char *str) {
    int rc;
 
+#ifdef PRETENDNOCAP
+   PUTT("\n%s%s%.*s %s%s%s"
+#else
    PUTT("%s%s%.*s %s%s%s"
+#endif
       , tg2(0, Msg_row)
       , Curwin->capclr_pmt
       , Screen_cols - 2
