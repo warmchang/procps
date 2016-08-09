530d19f90f5d99ee6e001ad061db3a18c42c1d1c
Jim Warner <james.warner@comcast.net>
2016-08-09 05:55:55 -0500

    library <STAT>: exclude an #include for --disable-numa
    When the numa stuff was imported from the top program,
    that #include for dlopen() was not made conditional as
    it should have been. Well, here it is being corrected.
    
    Signed-off-by: Jim Warner <james.warner@comcast.net>

--- proc/stat.c
+++ proc/stat.c
@@ -16,7 +16,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#ifndef NUMA_DISABLE
 #include <dlfcn.h>
+#endif
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
