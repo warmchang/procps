4e77b307dcae2de05c508d8742dd5e7597050754
Jim Warner <james.warner@comcast.net>
2018-01-26 00:00:00 -0600

    top: avoid potential truncation with 'Inspect' feature
    As it turns out, that Ukrainian 'demo' text supporting
    the '=' command was 152 bytes long, up from an English
    version of 80 bytes. Unfortunately, the buffer used to
    format all such strings was insufficient at 128 bytes.
    
    Depending on the width of one's terminal, some strange
    result could be experienced when a multi-byte sequence
    was truncated. So, this just makes that buffer bigger.
    
    Signed-off-by: Jim Warner <james.warner@comcast.net>

--- top/top.c
+++ top/top.c
@@ -3339,7 +3339,7 @@ static int insp_view_choice (proc_t *obj) {
  #define makFS(dst) { if (Insp_sel->flen < 22) \
        snprintf(dst, sizeof(dst), "%s", Insp_sel->fstr); \
     else snprintf(dst, sizeof(dst), "%.19s...", Insp_sel->fstr); }
-   char buf[SMLBUFSIZ];
+   char buf[LRGBUFSIZ];
    int key, curlin = 0, curcol = 0;
 
 signify_that:
