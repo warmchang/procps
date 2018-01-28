52f0ee2c41a12410fa2a6298f6592c4842c483c7
Jim Warner <james.warner@comcast.net>
2018-01-28 00:00:00 -0600

    top: update copyright dates in source and man document
    Signed-off-by: Jim Warner <james.warner@comcast.net>

--- top/top_nls.h
+++ top/top_nls.h
@@ -1,8 +1,6 @@
 /* top_nls.h - provide the basis for future nls translations */
 /*
- * Copyright (c) 2011-2017, by: James C. Warner
- *    All rights reserved.      8921 Hilloway Road
- *                              Eden Prairie, Minnesota 55347 USA
+ * Copyright (c) 2011-2018, by: James C. Warner
  *
  * This file may be used subject to the terms and conditions of the
  * GNU Library General Public License Version 2, or any later version
