ede4a5f9b050e797582dfbde748a5cc3b4d072fb
Jim Warner <james.warner@comcast.net>
2018-07-25 00:00:00 -0500

    top: existing 'Inspect' pipe feature now more flexible
    Currently, it isn't possible to establish an 'Inspect'
    pipe that relies on SIGINT to end. That's because this
    signal will also end the parent process (top) as well.
    
    So this patch will temporarily ignore that signal when
    processing any 'Inspect' pipe, allowing one like this:
    
    . pipe ^I Trace Calls ^I /usr/bin/strace -r -p %d 2>&1
    
    Signed-off-by: Jim Warner <james.warner@comcast.net>

--- top/top_nls.h
+++ top/top_nls.h
@@ -89,7 +89,7 @@ enum norm_nls {
    YINSP_deqtyp_txt, YINSP_dstory_txt,
 #endif
    YINSP_failed_fmt, YINSP_noents_txt, YINSP_pidbad_fmt, YINSP_pidsee_fmt,
-   YINSP_status_fmt, YINSP_workin_txt,
+   YINSP_status_fmt, YINSP_waitin_txt, YINSP_workin_txt,
       norm_MAX
 };
 
