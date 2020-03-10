2d9428222408cd4085a0eb2c04f3935272ed90d5
Jim Warner <james.warner@comcast.net>
2020-03-10 00:00:00 -0500

    top: eliminate inadvertent trailing double semi-colons
    Signed-off-by: Jim Warner <james.warner@comcast.net>

--- top/top.c
+++ top/top.c
@@ -3241,7 +3241,7 @@ static inline void insp_show_pgs (int col, int row, int max) {
    void (*mkrow_func)(int, int);
    int r = snprintf(buf, sizeof(buf), "%d", Insp_nl);
    int c = snprintf(buf, sizeof(buf), "%d", col +Screen_cols);
-   int l = row +1, ls = Insp_nl;;
+   int l = row +1, ls = Insp_nl;
 
    if (!Insp_bufrd)
       l = ls = 0;
@@ -5246,7 +5246,7 @@ static void keys_summary (int ch) {
          if (!CHKw(w, View_MEMORY))
             SETw(w, View_MEMORY);
          else if (++w->rc.graph_mems > 2) {
-            w->rc.graph_mems = 0;;
+            w->rc.graph_mems = 0;
             OFFw(w, View_MEMORY);
          }
          break;
@@ -5254,7 +5254,7 @@ static void keys_summary (int ch) {
          if (!CHKw(w, View_STATES))
             SETw(w, View_STATES);
          else if (++w->rc.graph_cpus > 2) {
-            w->rc.graph_cpus = 0;;
+            w->rc.graph_cpus = 0;
             OFFw(w, View_STATES);
          }
          break;
