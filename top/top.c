cc3c2b773d167194106c184af7617342ad86431a
Jim Warner <james.warner@comcast.net>
2022-06-15 00:00:00 -0500

    top: program is now more responsive to keyboard inputs <=== port of newlib 3ea1bc77
    ______________________________ original newlib message
    --------------------------------------------- ( none )
    
    Signed-off-by: Jim Warner <james.warner@comcast.net>

--- top/top.c
+++ top/top.c
@@ -2094,7 +2094,6 @@ static void adj_geometry (void) {
    Graph_adj = (float)Graph_len / 100.0;
 
    fflush(stdout);
-   Frames_signal = BREAK_off;
 } // end: adj_geometry
 
 
@@ -7182,6 +7181,7 @@ int main (int argc, char *argv[]) {
 
       if (0 < Loops) --Loops;
       if (!Loops) bye_bye(NULL);
+      if (Frames_signal) { Frames_signal = BREAK_off; continue; }
 
       ts.tv_sec = Rc.delay_time;
       ts.tv_nsec = (Rc.delay_time - (int)Rc.delay_time) * 1000000000;
