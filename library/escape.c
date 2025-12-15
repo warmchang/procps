3dcb441bea4f9c7ac8c1a1b32fac5d238e67b7ad
Jim Warner <james.warner@comcast.net>
2025-12-15 00:00:00 -0600

    library: reduce function call overhead for utf-8 logic
    The utf-8 validation code found in the escape.c module
    is structured so the u8charlen function was called for
    every 'character' (ascii or multi-byte) in the string.
    
    This commit just transforms the code in u8charlen into
    a loop so that it will be called just once per string.
    
    [ when tested with top, using a consistent number of ]
    [ tasks and rcfile specifying all fields requiring a ]
    [ call to escape_str, performance improved about 6%. ]
    
    Reference(s):
    May, 2024 - escape refactored, u8charlen introduced
    commit 73d332e8f174d51ddf44a14573bac623601e8c00
    
    Signed-off-by: Jim Warner <james.warner@comcast.net>

--- library/escape.c
+++ library/escape.c
@@ -1,7 +1,7 @@
 /*
  * escape.c - printing handling
  *
- * Copyright © 2011-2024 Jim Warner <james.warner@comcast.net>
+ * Copyright © 2011-2025 Jim Warner <james.warner@comcast.net>
  * Copyright © 2016-2024 Craig Small <csmall@dropbear.xyz>
  * Copyright © 1998-2005 Albert Cahalan
  *
@@ -37,51 +37,69 @@
 
 
 /*
- * Return the number of bytes this UTF-8 string uses
- * Compliant with RFC 3629
+ * Validate a UTF-8 string, with some characters possibly escaped,
+ * while remaining compliant with RFC 3629
  *
- * Returns how many bytes or -1 if invalid
+ * FIXME: not future-proof
  */
-// FIXME: not future-proof
-static int u8charlen (const unsigned char *s, unsigned size) {
+static void u8charlen (unsigned char *s, unsigned size) {
+   int n;
    unsigned x;
 
-   if (! size) return 0;
-
-   // 0xxxxxxx, U+0000 - U+007F
-   if (s[0] <= 0x7f) return 1;
-   if (size >= 2 && (s[1] & 0xc0) == 0x80) {
-      // 110xxxxx 10xxxxxx, U+0080 - U+07FF
-      if (s[0] >= 0xc2 && s[0] <= 0xdf) return 2;
-      if (size >= 3 && (s[2] & 0xc0) == 0x80) {
+   while (size) {
+      // 0xxxxxxx, U+0000 - U+007F
+      if (s[0] <= 0x7f) { n = 1; goto esc_maybe; }
+      if (size >= 2 && (s[1] & 0xc0) == 0x80) {
+         // 110xxxxx 10xxxxxx, U+0080 - U+07FF
+         if (s[0] >= 0xc2 && s[0] <= 0xdf) { n = 2; goto next_up; };
+         if (size >= 3 && (s[2] & 0xc0) == 0x80) {
 #ifndef OFF_UNICODE_PUA
-         x = ((unsigned)s[0] << 16) + ((unsigned)s[1] << 8) + (unsigned)s[2];
-         /* 11101110 10000000 10000000, U+E000 - primary PUA begin
-            11101111 10100011 10111111, U+F8FF - primary PUA end */
-         if (x >= 0xee8080 && x <= 0xefa3bf) return -1;
+            x = ((unsigned)s[0] << 16) + ((unsigned)s[1] << 8) + (unsigned)s[2];
+            /* 11101110 10000000 10000000, U+E000 - primary PUA begin
+               11101111 10100011 10111111, U+F8FF - primary PUA end */
+            if (x >= 0xee8080 && x <= 0xefa3bf) goto esc_definitely;
 #endif
-         x = (unsigned)s[0] << 6 | (s[1] & 0x3f);
-         // 1110xxxx 10xxxxxx 10xxxxxx, U+0800 - U+FFFF minus U+D800 - U+DFFF
-         if ((x >= 0x3820 && x <= 0x3b5f) || (x >= 0x3b80 && x <= 0x3bff)) return 3;
-         if (size >= 4 && (s[3] & 0xc0) == 0x80) {
+            x = (unsigned)s[0] << 6 | (s[1] & 0x3f);
+            // 1110xxxx 10xxxxxx 10xxxxxx, U+0800 - U+FFFF minus U+D800 - U+DFFF
+            if ((x >= 0x3820 && x <= 0x3b5f) || (x >= 0x3b80 && x <= 0x3bff)) { n = 3; goto next_up; };
+            if (size >= 4 && (s[3] & 0xc0) == 0x80) {
 #ifndef OFF_UNICODE_PUA
-            unsigned y;
-            y = ((unsigned)s[0] << 24) + ((unsigned)s[1] << 16) + ((unsigned)s[2] << 8) + (unsigned)s[3];
-            /* 11110011 10110000 10000000 10000000, U+F0000  - supplemental PUA-A begin
-               11110011 10111111 10111111 10111101, U+FFFFD  - supplemental PUA-A end */
-            if (y >= 0xf3b08080 && y <= 0Xf3bfbfbd) return -1;
-            /* 11110100 10000000 10000000 10000000, U+100000 - supplemental PUA-B begin
-               11110100 10001111 10111111 10111101, U+10FFFD - supplemental PUA-B end */
-            if (y >= 0xf4808080 && y <= 0Xf48fbfbd) return -1;
+               unsigned y;
+               y = ((unsigned)s[0] << 24) + ((unsigned)s[1] << 16) + ((unsigned)s[2] << 8) + (unsigned)s[3];
+               /* 11110011 10110000 10000000 10000000, U+F0000  - supplemental PUA-A begin
+                  11110011 10111111 10111111 10111101, U+FFFFD  - supplemental PUA-A end */
+               if (y >= 0xf3b08080 && y <= 0Xf3bfbfbd) goto esc_definitely;
+               /* 11110100 10000000 10000000 10000000, U+100000 - supplemental PUA-B begin
+                  11110100 10001111 10111111 10111101, U+10FFFD - supplemental PUA-B end */
+               if (y >= 0xf4808080 && y <= 0Xf48fbfbd) goto esc_definitely;
 #endif
-            // 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx, U+010000 - U+10FFFF
-            if (x >= 0x3c10 && x <= 0x3d0f) return 4;
+               // 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx, U+010000 - U+10FFFF
+               if (x >= 0x3c10 && x <= 0x3d0f) { n = 4; goto next_up; };
+            }
          }
       }
+      // invalid or incomplete sequence
+esc_definitely:
+      n = -1;
+      // fall through
+esc_maybe:
+      /*  Escape the character to a '?' if
+       *  Not valid UTF (when n is -1)
+       *  Non-control chars below SPACE
+       *  DEL
+       *  32 unicode multibyte control characters which begin at U+0080 (0xc280)
+       */
+      if ((n < 0)
+      || ((s[0] < 0x20)
+      || ((s[0] == 0x7f)
+      || ((s[0] == 0xc2 && s[1] >= 0x80 && s[1] <= 0x9f)))))
+         *s = '?';
+      n = 1;
+      // fall through
+next_up:
+      s += n;
+      size -= n;
    }
-
-   // invalid or incomplete sequence
-   return -1;
 }
 
 /*
@@ -92,33 +110,6 @@ static inline void esc_all (unsigned char *str) {
       if (!isprint(*str))
           *str = '?';
       ++str;
-   }
-}
-
-static inline void esc_ctl (unsigned char *str, int len) {
-   int n;
-
-   if (len <= 0)
-      return;
-
-   while ((n = u8charlen(str, len) )) {
-      /* Escape the character to a '?' if
-       *  Not valid UTF so charlen is -1
-       *  Non-control chars below SPACE
-       *  DEL
-       *  32 unicode multibyte control characters which begin at U+0080 (0xc280)
-       */
-      if (
-         n < 0
-         || str[0] < 0x20
-         || str[0] == 0x7f
-         || (str[0] == 0xc2 && str[1] >= 0x80 && str[1] <= 0x9f)) {
-
-         *str = '?';
-         n = 1;
-      }
-      str += n;
-      len -= n;
    }
 }
 
@@ -140,7 +131,7 @@ int escape_str (char *dst, const char *src, int bufsize) {
    if (utf_sw < 0)
       esc_all((unsigned char *)dst);
    else
-      esc_ctl((unsigned char *)dst, n);
+      u8charlen((unsigned char *)dst, n);
    return n;
 }
 
